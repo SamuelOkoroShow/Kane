9448178cd9b5404ef51f09cbd075879343541779
Samuel Okoro <samuel.okoro@fxdigital.uk>
2022-05-22 01:39:56 +0100

    Bitbucket Upgrade SSH

---
+++ app/ui/outgoingf.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function outgoing() {
+  return (
+    <View>
+      <Text>outgoingf</Text>
+    </View>
+  )
+}
