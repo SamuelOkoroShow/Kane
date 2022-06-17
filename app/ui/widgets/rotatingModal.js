02625f806697ad04af200240e10cba63e383b15d
Samuel Okoro <s.okoro07@gmail.com>
2022-06-17 10:53:42 -0500

    feat: Init Modal inferstructure

---
+++ app/ui/widgets/rotatingModal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Platform} from "react-native"
+
+import styled from 'styled-components/native'
+
+const StyledView = styled.View`
+  flex: 1;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items:center;
+`
+const Modal = styled.View`
+  width: 200px;
+  height: 100px;
+  border-radius: 5px;
+  background-color: tomato;
+`
+
+const ModalH1 = styled.Text`
+  color: white;
+  font-weight: Platform.OS === "android"? bold:  900;
+  font-size: 26;
+`
+
+export default function rotatingModal() {
+  return <StyledView>
+    <Modal>
+      
+    </Modal>
+  </StyledView>;
+}
---
+++ app/ui/widgets/standardModalTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from "react-native"
+
+import styled from 'styled-components/native'
+
+const StyledView = styled.View`
+  flex: 1;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items:center;
+`
+const Modal = styled.View`
+  
+`
+
+
+
+export default function rotatingModal() {
+  return <StyledView />;
+}
